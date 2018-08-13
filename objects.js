bc42360b263ba07c932fd287c6342a9cd30d82f1
zhifengou@hotmail.com <zhifengou@hotmail.com>
2018-08-13 19:19:37 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,7 +3,11 @@ function updatePlaylist(playlist, artistName, songTitle){
    playlist[artistName] = songTitle
   return playlist
 }
+<<<<<<< HEAD
 function removeFromPlaylist(playlist,artistName){
  delete playlist[artistName]
  return playlist
 }
+=======
+function
+>>>>>>> 919d2a9b227d2ba955bd45ae28898d1ac1b83b3b
